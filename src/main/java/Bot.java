332ac6acb5cd2977a0cd8936fdc211334ec885aa
Evgenii Vlasov <evjvlasov@gmail.com>
2019-10-10 19:36:45 +0300

    Bot1

---
+++ src/main/java/Bot.java
@@ -0,0 +1,56 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if ("/help".equals(message.getText())) {
+                sendAnswer(message, "Help");
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "Oxford_Dictionary_Bot";
+    }
+
+    public String getBotToken() {
+        return "966644970:AAEKfe9up9KqXL4G6o9oxPO0IBNTg0Lb9x0";
+    }
+
+    public void sendAnswer(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
